dcb6930895e362133e17c141550f3de041db1e26
The-Unknown-Developer <theunknowndevloper@gmail.com>
2021-02-05 17:19:00 -0500

    crags list categories datat

--- src/componets/gallery.js
+++ src/componets/gallery.js
@@ -4,37 +4,34 @@ export default class Gallery extends Component {
   constructor() {
     super();
     this.state = {
-      allimgs:  [''],
+      allimgs: [""],
       currentImg: "",
-      currentIndex: 0
-      
+      currentIndex: 0,
     };
 
     this.allImgsLoop = this.allImgsLoop.bind(this);
     this.componentDidMount = this.componentDidMount.bind(this);
   }
-  componentDidMount(){
-      let allimg =    [
-        "https://images.craigslist.org/00Z0Z_4WGWlf4RjV5z_0yQ0t2_600x450.jpg",
-        "https://images.craigslist.org/00J0J_cN7cbVBHL8mz_0Aw0t2_600x450.jpg",
-        "https://images.craigslist.org/00z0z_lxTx6peQQK2z_0t20CI_1200x900.jpg",
-        "https://images.craigslist.org/00e0e_iCpJCKnNepEz_0CI0t2_1200x900.jpg",
-      ]
+  componentDidMount() {
+    let allimg = [
+      "https://images.craigslist.org/00Z0Z_4WGWlf4RjV5z_0yQ0t2_600x450.jpg",
+      "https://images.craigslist.org/00J0J_cN7cbVBHL8mz_0Aw0t2_600x450.jpg",
+      "https://images.craigslist.org/00z0z_lxTx6peQQK2z_0t20CI_1200x900.jpg",
+      "https://images.craigslist.org/00e0e_iCpJCKnNepEz_0CI0t2_1200x900.jpg",
+    ];
 
-      this.setState({
-           allimgs: allimg,
-          currentImg: allimg[this.state.currentImg]
-
-      })
+    this.setState({
+      allimgs: allimg,
+      currentImg: allimg[this.state.currentImg],
+    });
   }
 
-  
+  // image loop
 
- 
   allImgsLoop = () => {
     return this.state.allimgs.map((item, i) => {
       return (
-        <div key={i} className="thumb-img">
+        <div onClick={this.clickedThumb.bind(null, i)} key={i} className="thumb-img">
           <img
             style={{
               backgroundImage: `url('${item}')`,
@@ -45,50 +42,52 @@ export default class Gallery extends Component {
     });
   };
 
+  //   slider
   nextBtn = () => {
-      
-
-    if(this.state.currentIndex !== (this.state.allimgs.length - 1) ) {
-        return this.setState({
-            currentIndex: this.state.currentIndex   +  1
-        })
-
-
+    if (this.state.currentIndex !== this.state.allimgs.length - 1) {
+      return this.setState({
+        currentIndex: this.state.currentIndex + 1,
+      });
     }
-    
-       
-     
+  };
+  // Slider
+  prevBtn = () => {
+    console.log("preevvv");
+    if (this.state.currentIndex !== 0) {
+      return this.setState({
+        currentIndex: this.state.currentIndex - 1,
+      });
+    }
+  };
 
- 
+
+  clickedThumb   = (index) => {
+
+    this.setState({
+        currentIndex: index 
+    })
 
   }
 
-  prevBtn = () => {
 
-    console.log("preevvv")
-    if(this.state.currentIndex !== 0 ) {
-        return this.setState({
-            currentIndex: this.state.currentIndex  -  1
-        })
 
-    }
-         
 
-      
-    
-      
-}
   render() {
     return (
       <div>
         <div className="main-image">
-          <div onClick={this.prevBtn} className="arrows left-arrow">{"<"}</div>
-          <div onClick={this.nextBtn}className=" arrows right-arrow">{">"}</div>
+          <div onClick={this.prevBtn} className="arrows left-arrow">
+            {"<"}
+          </div>
+          <div onClick={this.nextBtn} className=" arrows right-arrow">
+            {">"}
+          </div>
           <div
             className="image-1"
             style={{
-              backgroundImage:
-                `url('${this.state.allimgs[this.state.currentIndex]}')`,
+              backgroundImage: `url('${
+                this.state.allimgs[this.state.currentIndex]
+              }')`,
             }}
           ></div>
         </div>
---
+++ src/data/categories.js
@@ -0,0 +1,327 @@
+module.exports = [
+    {
+        title: "community",
+
+        listings: [ 
+            {
+                name: "activities",
+                slug: "activities",
+
+            },
+            {
+                name: "lost/found",
+                slug: "lost-found",
+
+            },
+            {
+                name: "artist",
+                slug: "artist",
+
+            },
+            {
+                name: "childcare",
+                slug: "chilcare",
+
+            },
+
+            {
+                name: "classes",
+                slug: "classes",
+
+            },
+            {
+                name: "events",
+                slug: "evemts",
+
+            },
+            {
+                name: "general",
+                slug: "general",
+
+            },
+            {
+                name: "groups",
+                slug: "groups",
+
+            },
+
+            {
+                name: "local news",
+                slug: "Local-news",
+
+            }
+        
+        ],
+
+        title: "housing",
+
+        listings: [ 
+            {
+                name: "apts/housing",
+                slug: "apts-housing",
+
+            },
+            {
+                name: "hosung swap",
+                slug: "housing-swap",
+
+            },
+            {
+                name: "house wanted",
+                slug: "house-wanted",
+
+            },
+            {
+                name: "office / commercial",
+                slug: "office-commercial",
+
+            },
+
+            {
+                name: "parking / storage",
+                slug: "parking-storage",
+
+            },
+            {
+                name: "real estate for sale",
+                slug: "real-estate-for-sale",
+
+            },
+            {
+                name: "rooms/shared",
+                slug: "rooms-shared",
+
+            },
+            {
+                name: "rooms wannted ",
+                slug: "rooms-wanted",
+
+            },
+
+            {
+                name: "vacation rentals",
+                slug: "vaction-rentals",
+
+            }
+        
+        ], 
+
+        title: "services",
+
+        listings: [ 
+            {
+                name: "automotive",
+                slug: "automotive",
+
+            },
+            {
+                name: "beauty",
+                slug: "beauty",
+
+            },
+            {
+                name: "cell/mobil",
+                slug: "cell-mobil",
+
+            },
+            {
+                name: "computer",
+                slug: "computer",
+
+            },
+
+            {
+                name: "cycle",
+                slug: "cycle",
+
+            },
+            {
+                name: "event",
+                slug: "event",
+
+            },
+            {
+                name: "farm+garden",
+                slug: "farm-garden",
+
+            },
+            {
+                name: "financial ",
+                slug: "financial",
+
+            },
+
+            {
+                name: "pet",
+                slug: "pet",
+
+            }
+        
+        ],
+        title: "jobs",
+
+        listings: [ 
+            {
+                name: "accounting+finance",
+                slug: "accounting/finance",
+
+            },
+            {
+                name: "admin/office",
+                slug: "admin-office",
+
+            },
+            {
+                name: "arch/engineering",
+                slug: "arch-engineering"
+
+            },
+            {
+                name: "art / media / design ",
+                slug: "art-media-design",
+
+            },
+
+            {
+                name: "bioteach / science ",
+                slug: "biotech-science",
+
+            },
+            {
+                name: "business/mgmt",
+                slug: "business-mgmt",
+
+            },
+            {
+                name: "custormer service",
+                slug: "customer-service",
+
+            },
+            {
+                name: "education",
+                slug: "education",
+
+            },
+
+            {
+                name: "etc / misc",
+                slug: "etc-misc",
+
+            }
+        
+        ],
+
+
+        title: "for sale",
+
+        listings: [ 
+            {
+                name: "antiques",
+                slug: "antiques",
+
+            },
+            {
+                name: "arts+crafts",
+                slug: "arts-crafts",
+
+            },
+            {
+                name: "atv/utv/sno",
+                slug: "atv-utv-sno"
+
+            },
+            {
+                name: "auto parts ",
+                slug: "auto-parts",
+
+            },
+
+            {
+                name: "baby+kids ",
+                slug: "baby-kids",
+
+            },
+            {
+                name: "barter",
+                slug: "barter",
+
+            },
+            {
+                name: "beauty+health",
+                slug: "beauty-health",
+
+            },
+            {
+                name: "bike parts",
+                slug: "bike-parts",
+
+            },
+
+            {
+                name: "boat parts",
+                slug: "boat-parts",
+
+            }
+        
+        ],
+
+        title: "gigs",
+
+        listings: [ 
+            {
+                name: "computer",
+                slug: "computer",
+
+            },
+            {
+                name: "creative",
+                slug: "creative",
+
+            },
+            {
+                name: "crew",
+                slug: "crew"
+
+            },
+            {
+                name: "domestic ",
+                slug: "domestic",
+
+            },
+
+            {
+                name: "event",
+                slug: "event",
+
+            },
+            {
+                name: "business/mgmt",
+                slug: "business-mgmt",
+
+            },
+            {
+                name: "custormer service",
+                slug: "customer-service",
+
+            },
+            {
+                name: "education",
+                slug: "education",
+
+            },
+
+            {
+                name: "etc / misc",
+                slug: "etc-misc",
+
+            }
+        
+        ]
+        
+        
+        
+
+        
+    
+    }
+]
